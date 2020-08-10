b2345b5d75a71b4b296774cdc37d7844faa6c51c
eva <eva.rum@gmx.de>
2020-08-10 16:36:11 +0200

    data_to_cut (commented) in helper.py

--- flaskr/flask_app/helper.py
+++ flaskr/flask_app/helper.py
@@ -1,6 +1,6 @@
 import os
 
-from flaskr.flask_app.config import PATH, PIC_ENDUNG
+from flaskr.flask_app.config import PATH, PIC_ENDUNG, VID_ENDUNG
 
 
 def get_file_meta(filename):
@@ -12,6 +12,7 @@ def get_file_meta(filename):
     return meta
 
 
+
 def data_to_html_file():
     direc = PATH
     ext = PIC_ENDUNG
@@ -20,11 +21,6 @@ def data_to_html_file():
         if os.path.splitext(i)[1] == ext:
             meta = get_file_meta(i)
             files.append(meta)
-
-    #file_loader = FileSystemLoader('templates')
-    #env = Environment(loader=file_loader)
-    #template = env.get_template('showimage.jinja')
-
     return files
 
 def filenames_split(video):
@@ -33,3 +29,33 @@ def filenames_split(video):
     cut = cut.split('-')
     cut = cut[1]
     return cut
+
+def data_to_cut(end):
+    ext = end
+    direc = PATH
+    #liste aller Elemente im Path-Ordner (filenames not sorted)
+    elements_list = []
+    for i in os.listdir(direc):
+        if os.path.splitext(i)[1] == ext:
+            elements_list.append(i)
+    #Dictionary to store evantual date/time together with filename
+    cut_elements = {}
+    for element in elements_list:
+        #filename shortened to date/time
+        cut_element = filenames_split(element)
+        #Dictionary-eintrag cut_element(date/time): element(filename)
+        cut_elements[cut_element] = element
+    #keys of cut_elements to list
+    list_cut_element = list(cut_elements.keys())
+    #sorted list new to old
+    list_cut_element.sort(reverse = True)
+    #filenames will be sorted list of cut_elements' values
+    filenames = []
+    for i in list_cut_element:
+        #element_value list filled with responding values to list_cut_element
+        element_value = cut_elements[i]
+        filenames.append(element_value)
+    print(filenames)
+    return filenames
+
+data_to_cut(VID_ENDUNG)
