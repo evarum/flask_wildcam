2a1e6e78faf687f7bf9c992d48546de9e130a155
eva <eva.rum@gmx.de>
2020-08-11 11:38:06 +0200

    list comparison functional(commented eng), but not yet integrated into 'real' code

--- flaskr/flask_app/helper.py
+++ flaskr/flask_app/helper.py
@@ -57,4 +57,26 @@ def data_to_cut(end):
     print(filenames)
     return filenames
 
-data_to_html_file(PIC_ENDUNG)
+# two sorted lists are compared and written down if they fullfill the following condition: lower_limit <= element_between < upper_limit
+def paar_sort(list1, list2):
+    # limits_list sets the borders of possible placement for elements of liste_between
+    limits_list = list1
+    liste_between = list2
+    paar = []
+    # iteration through limits_list
+    for lower_limit in limits_list:
+       # print(lower_limit)
+        for element_between in liste_between:
+            # is element_between bigger/equal than the lower_limit?
+            if element_between >= lower_limit:
+                # defining upper_limit
+                upper_limit = lower_limit+1
+                # is element_between bigger than the upper_limit?
+                if element_between > upper_limit:
+                    # exit loop and go to next element_between
+                    break
+                # appending lower_limit and element_between to list paar
+                paar.append([lower_limit,element_between])
+    print(paar)
+    # giving back the finished sets
+    return paar
