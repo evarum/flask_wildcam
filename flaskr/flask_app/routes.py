216640c6f4dca855da41468af45d8b4b83fca81b
eva <eva.rum@gmx.de>
2020-08-12 11:59:28 +0200

    added more comments (eng)

--- flaskr/flask_app/routes.py
+++ flaskr/flask_app/routes.py
@@ -3,16 +3,15 @@ from flaskr.flask_app.main import app
 from flask import (render_template, send_from_directory)
 from flaskr.flask_app.helper import data_to_html_file
 
-#
 @app.route('/')
 @app.route('/index')
 def index( *args):
-    # defines the subheading
     heading = 'Filme'
     files = data_to_html_file()
+    # render template with filled in data
     return render_template('test_wc.html',files=files, heading=heading)
 
-#
+# path for pictures is specified
 @app.route('/images/<path:filename>')
 def images(filename):
     return send_from_directory(PATH, filename)
--- flaskr/flask_app/video_routes.py
+++ flaskr/flask_app/video_routes.py
@@ -2,12 +2,13 @@ from flask import (render_template)
 from flaskr.flask_app.helper import get_file_meta
 from flaskr.flask_app.main import app
 
-#
+# path for videos is specified
 @app.route('/videos/<path:filename>')
+
+# function renders template with filled in data (for videos)
 def videos(filename):
-    heading = 'Filme'
     file = get_file_meta(filename)
     file['video'] = filename
-    return render_template('video.html',file=file, heading=heading)
+    return render_template('video.html',file=file)
 
 
